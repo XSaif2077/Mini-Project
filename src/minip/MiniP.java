b03a5fd45044f41459c87faca00973f91c3b847b
Saif Ahmed Digonto <saif.ahmed122519@gmail.com>
2024-11-13 21:20:30 +0600

    Update MiniP.java

--- src/minip/MiniP.java
+++ src/minip/MiniP.java
@@ -4,6 +4,25 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
 
+class Product {
+    private String productId;
+    private String productName;
+    private String description;
+    private String size;
+    private String color;
+    private double price;
+    private int stock;
+
+    public Product(String productId, String productName, String description, String size, String color, double price, int stock) {
+        this.productId = productId;
+        this.productName = productName;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stock = stock;
+    }
+
 public class MiniP {
 
     public static void main(String[] args) {
